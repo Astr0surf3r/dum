fc0d511a17b4d6e0701b46eb02e448fd021665ea
Astr0surf3r <astr0surf3r@gmail.com>
2012-03-30 14:54:43 -0700

    primero git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dum::Application.config.secret_token = '484645a718c118c99b1b277779a4e13bfffa3a28987dea777862e257102fe52226b027552ac2de8b0b0465d75727fb8b31af79259ba60c6752283bba03857c7b'
